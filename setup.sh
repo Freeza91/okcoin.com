7158f1e54dfb14933f1ebaa381fd4165328869bc
rudy <347212291@qq.com>
2017-04-19 00:04:13 +0800

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export PYTHONPATH="$PWD/libs"
-# if [ -d $("$HOME/.virtualenvs/py3") ]; then
-if [ -d "$HOME/.virtualenvs/py3" ]; then
-  workon py3
-else
-  echo "you need install python3"
-fi
+
+# if [ -d "$HOME/.virtualenvs/py3" ]; then
+#   workon py3
+# else
+#   echo "you need install python3"
+# fi
 
