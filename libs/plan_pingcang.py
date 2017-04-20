0d84459e405ae4205f6b118c1ff6d66e5d6e9f5f
rudy <347212291@qq.com>
2017-04-20 21:36:17 +0800

    add logging

--- libs/plan_pingcang.py
+++ libs/plan_pingcang.py
@@ -2,19 +2,26 @@
 
 import logging
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='./trade.log',
+                    filemode='w')
+
+def trade(okcoin, _type, amount):
+    okcoin.future_trade('btc_usd','quarter','',amount, _type,'1','10')
+    if _type == '3':
+        category = '平多'
+    elif _type == '4':
+        category = '平空'
+
+    logging.info('以%s价格平%s：数量%s' % (current_price, category, amount))
+
 # 对手价
 def plan(trend, current_price, plan_price, amount, okcoin, _type):
     if trend == '>=':
         if current_price >= plan_price:
-            okcoin.future_trade('btc_usd','quarter','',amount, _type,'1','10')
-            logging.info('以%s价格平仓' % current_price)
+            trade(okcoin, _type, amount)
     elif trend == '<=':
         if current_price <= plan_price:
-            okcoin.future_trade('btc_usd','quarter','',amount, _type,'1','10')
-            logging.info('以%s价格平仓' % current_price)
-
-# print (u'期货逐仓账户信息')
-# print (okcoinFuture.future_userinfo_4fix())
-
-# print (u'期货逐仓持仓信息')
-# print (okcoinFuture.future_position_4fix('btc_usd','quarter',1))
+            trade(okcoin, _type, amount)
